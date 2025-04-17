60cf75ea63fadeff3fa2ade992f06773a4fa694c
hieurury <hieurury007@gmail.com>
2025-04-17 22:28:58 +0700

    Initial commit

---
+++ src/main/java/fabric/hieurury/mobarise/MobAriseClient.java
@@ -0,0 +1,11 @@
+package fabric.hieurury.mobarise;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MobAriseClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
+
---
+++ src/main/java/fabric/hieurury/mobarise/MobAriseDataGenerator.java
@@ -0,0 +1,11 @@
+package fabric.hieurury.mobarise;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class MobAriseDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
